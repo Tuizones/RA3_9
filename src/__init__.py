b17cf83dd7040aec6f2fa8d07c9ddb4a90761b6f
Arthur Biancolini <arthurbach57@gmail.com>
2026-05-23 23:43:13 -0300

    implementar prepararEntradaSemantica

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pipeline import (
-    parseExpressao as parseExpressao,
-    executarExpressao as executarExpressao,
-    gerarAssembly as gerarAssembly,
-    exibirResultados as exibirResultados,
-    lerArquivo as lerArquivo,
-)
